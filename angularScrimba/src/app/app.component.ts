da37a1eaa6297af7b9f6fae13ef144fb75d837de
Vinz <vincentballut@outlook.fr>
2019-10-01 09:34:44 +0200

    First hello world

--- angularScrimba/src/app/app.component.ts
+++ angularScrimba/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  template: "<h1>{{title}}</h1>"
 })
-export class AppComponent {
-  title = 'angularScrimba';
+export class AppComponent implements OnInit {
+  title: string;
+  constructor() {}
+  ngOnInit() {
+    //we call a service that gets us the data
+    this.title = "Hello world";
+  }
 }
